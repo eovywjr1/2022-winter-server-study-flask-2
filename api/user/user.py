593f95e5ff732798aa8e99882ad499e9b400f224
JoMinsu <eovywjr1@naver.com>
2023-02-05 18:32:40 +0900

    v_11

--- api/user/user.py
+++ api/user/user.py
@@ -3,12 +3,13 @@ from flask_restx import Resource, Namespace
 from database.database import Database
 
 user = Namespace('user')
-db = Database()
 
 @user.route('')
 class UserManagement(Resource):
     def get(self):
         """유저 데이터 조회"""
+        
+        db = Database()
         
         userInfo = request.args.to_dict()
         id = userInfo['id']
@@ -16,16 +17,27 @@ class UserManagement(Resource):
         
         sql = "SELECT * FROM user WHERE id = '" + id + "'"
         row = db.execute_one(sql)
-        if(row is None): return make_response(jsonify({'message' : '해당 유저가 존재하지 않음'}), 400)
+        if(row is None):
+            db.close()
+            
+            return make_response(jsonify({'message' : '해당 유저가 존재하지 않음'}), 400)
         else:
             sql = "SELECT * FROM user WHERE id = '" + id + "' and pw = '" + password + "'"
             row = db.execute_one(sql)
             
-            if(row is None): return make_response(jsonify({'message' : '아이디나 비밀번호 불일치'}), 400)
-            else: return make_response(jsonify({'nickname' : row['nickname']}), 200)
+            if(row is None): 
+                db.close()
+                
+                return make_response(jsonify({'message' : '아이디나 비밀번호 불일치'}), 400)
+            else: 
+                db.close()
+                
+                return make_response(jsonify({'nickname' : row['nickname']}), 200)
     
     def post(self):
         """유저 생성"""
+        
+        db = Database()
         
         userInfo = request.get_json()
         id = userInfo['id']
@@ -37,12 +49,19 @@ class UserManagement(Resource):
         if(row is None):
             sql = "INSERT INTO user VALUES ('" + id + "', '" + password + "', '" + nickname + "')"
             db.execute(sql)
+            db.commit()
+            db.close()
             
             return make_response(jsonify({'is_success' : True, 'message' : '유저 생성 성공'}), 200)
-        else: return make_response(jsonify({'is_success' : False, 'message' : '이미 있는 유저'}), 400)
+        else: 
+            db.close()
+            
+            return make_response(jsonify({'is_success' : False, 'message' : '이미 있는 유저'}), 400)
 
     def put(self):
         """유저 데이터(닉네임) 수정"""
+        
+        db = Database()
         
         userInfo = request.get_json()
         id = userInfo['id']
@@ -51,16 +70,26 @@ class UserManagement(Resource):
         
         sql = "SELECT * FROM user WHERE id = '" + id + "' and pw = '" + password + "'"
         row = db.execute_one(sql)
-        if(row is None): return make_response(jsonify({'is_success' : False, 'message' : '아이디나 비밀번호 불일치'}), 400)
-        elif(row['nickname'] == nickname): return make_response(jsonify({'is_success' : False, 'message' : '현재 닉네임과 같음'}), 400)
+        if(row is None): 
+            db.close()
+            
+            return make_response(jsonify({'is_success' : False, 'message' : '아이디나 비밀번호 불일치'}), 400)
+        elif(row['nickname'] == nickname): 
+            db.close()
+            
+            return make_response(jsonify({'is_success' : False, 'message' : '현재 닉네임과 같음'}), 400)
         else:
             sql = "UPDATE user SET nickname = '" + nickname + "' WHERE id = '" +  id + "'"
             db.execute(sql)
+            db.commit()
+            db.close()
             
             return make_response(jsonify({'is_success' : True, 'message' : '유저 닉네임 변경 성공'}), 200)
     
     def delete(self):
         """유저 삭제"""
+        
+        db = Database()
         
         userInfo = request.get_json()
         id = userInfo['id']
@@ -68,9 +97,14 @@ class UserManagement(Resource):
         
         sql = "SELECT * FROM user WHERE id = '" + id + "' and pw = '" + password + "'"
         row = db.execute_one(sql)
-        if(row is None): return make_response(jsonify({'is_success' : False, 'message' : '아이디나 비밀번호 불일치'}), 400)
+        if(row is None): 
+            db.close()
+            
+            return make_response(jsonify({'is_success' : False, 'message' : '아이디나 비밀번호 불일치'}), 400)
         else:
             sql = "DELETE FROM user WHERE id = '" + id + "'"
             db.execute(sql)
+            db.commit()
+            db.close()
             
             return make_response(jsonify({'is_success' : True, 'message' : '유저 삭제 성공'}), 200)
